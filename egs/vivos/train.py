7fd62bbd8c3d58aba097f0c5e8f360ebb3c47cb8
icon197 <tanb1509892@student.ctu.edu.vn>
2019-06-05 14:49:31 +0700

    update train.py

--- egs/vivos/train.py
+++ egs/vivos/train.py
@@ -1,6 +1,5 @@
-import os
 import sys
-sys.path.append(os.getcwd() + "/../..")
+sys.path.append("/content/automatic_speech_recognition")
 from egs.vivos.extension.model import KaldiSpeechRecognition
 from os.path import join, dirname
 import argparse
