33c1e582fdd067c0d6f484006f36a594437c5096
icon197 <tanb1509892@student.ctu.edu.vn>
2019-06-08 17:00:49 +0700

    update

--- egs/vivos/extension/model.py
+++ egs/vivos/extension/model.py
@@ -193,7 +193,7 @@ class KaldiSpeechRecognition:
             "first_beam=10.0",
             "beam=12.0",
             "lattice_beam=6.0",
-            "max-active-states=14000"
+            "max-active=14000"
         ]
         content = "\n".join(configs)
         outfile = "{}/conf/decode.config".format(self.tmp_folder)
@@ -231,7 +231,7 @@ class KaldiSpeechRecognition:
         train_text = open(train_text_file).read().splitlines()[:self.N_TRAIN]
         test_text_file = "{}/test/text".format(self.corpus_folder)
         test_text = open(test_text_file).read().splitlines()[:self.N_TEST]
-        plus_text_file = "{}/plus.txt".format(self.corpus_folder)
+        plus_text_file = "{}/plus2.txt".format(self.corpus_folder)
         plus_text = open(plus_text_file).read().splitlines()
         text = train_text + test_text
         text = [item.split("|")[1] for item in text]
