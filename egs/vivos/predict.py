98284db4d27fda50ca1bba3183cff9017f5832d6
icon197 <tanb1509892@student.ctu.edu.vn>
2019-06-14 14:18:43 +0700

    update

--- egs/vivos/predict.py
+++ egs/vivos/predict.py
@@ -124,10 +124,11 @@ def predict(kaldi_folder, wav_file, model_path, method="delta", utils_path=None)
                   ark:transcriptions/splice-transform-feats.ark".format(model, kaldi_folder))
         os.system("cd {}/predict; {}/src/sgmm2bin/sgmm2-latgen-faster \
                         --max-active=14000 --beam=12.0 --lattice_beam=6.0 --acoustic-scale=0.083333 --allow-partial=true\
-                          --word-symbol-table=experiment/triphones_deldel/graph/words.txt \
+                        --word-symbol-table=experiment/triphones_deldel/graph/words.txt \
+                        --gselect=ark,s,cs:gunzip -c {}/decode/gselect.1.gz \
                           experiment/triphones_deldel/final.mdl experiment/triphones_deldel/graph/HCLG.fst \
                           ark:transcriptions/splice-transform-feats.ark ark,t:transcriptions/lattices.ark" \
-                  .format(model, kaldi_folder))
+                  .format(model, model, kaldi_folder))
     else:
         raise Exception("The given method {} is not supported yet".format(method))
 
