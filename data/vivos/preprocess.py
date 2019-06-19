79abbc6540e648d266c5cda8c8933e399df71106
icon197 <tanb1509892@student.ctu.edu.vn>
2019-06-19 13:56:51 +0700

    update

--- data/vivos/preprocess.py
+++ data/vivos/preprocess.py
@@ -98,7 +98,7 @@ def create_speaker():
 
 
 def cpy_plus_text():
-    shutil.copy2("plus_big.txt", "corpus/plus.txt")
+    shutil.copy2("plus3.txt", "corpus/plus.txt")
     shutil.copy2("viet72k.txt", "corpus/viet72k.txt")
 
 try:
