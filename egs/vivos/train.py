24c46e59b3cf3af920d8ad8d3ab4eb12162b6f4b
icon197 <tanb1509892@student.ctu.edu.vn>
2019-06-24 15:19:52 +0700

    update

--- egs/vivos/train.py
+++ egs/vivos/train.py
@@ -20,7 +20,7 @@ def train(kaldi_folder, corpus_folder, export_folder=None, nj=1, method="deltade
     params = {
         "method": method,
         "jobs": nj,
-        "lm_order": 6
+        "lm_order": 5
     }
     model = KaldiSpeechRecognition(corpus_folder, kaldi_folder, params)
     model.fit()
